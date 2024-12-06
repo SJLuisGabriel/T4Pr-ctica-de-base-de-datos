ae427dc6086894b46d84fbe6e5d6a42401f35f8a
Ricardo Garcia Noguez <ricardo.garcia.noguez.1220@gmail.com>
2024-12-06 16:25:23 -0600

    feat: agregación de fonts y themes

--- lib/settings/theme_settings.dart
+++ lib/settings/theme_settings.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-class ModoOscuro {
-  static ThemeData get() {
-    return ThemeData(
+class ThemeSettings {
+  static ThemeData modoOscuro() {
+    return ThemeData.dark().copyWith(
       brightness: Brightness.dark,
       primaryColor: Colors.deepPurple,
       hintColor: Colors.purpleAccent,
@@ -58,7 +58,7 @@ class ModoOscuro {
           borderRadius: BorderRadius.circular(12),
         ),
         focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.purpleAccent),
+          borderSide: const BorderSide(color: Colors.purpleAccent),
           borderRadius: BorderRadius.circular(12),
         ),
         hintStyle: const TextStyle(color: Colors.white60),
@@ -80,11 +80,9 @@ class ModoOscuro {
       ),
     );
   }
-}
 
-class ModoLuz {
-  static ThemeData get() {
-    return ThemeData(
+  static ThemeData modoLuz() {
+    return ThemeData.light().copyWith(
       brightness: Brightness.light,
       primaryColor: Colors.blue,
       hintColor: Colors.lightBlueAccent,
@@ -139,7 +137,7 @@ class ModoLuz {
           borderRadius: BorderRadius.circular(12),
         ),
         focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.lightBlueAccent),
+          borderSide: const BorderSide(color: Colors.lightBlueAccent),
           borderRadius: BorderRadius.circular(12),
         ),
         hintStyle: const TextStyle(color: Colors.black38),
@@ -162,3 +160,166 @@ class ModoLuz {
     );
   }
 }
+
+// class ModoOscuro {
+//   static ThemeData get() {
+//     return ThemeData(
+//       brightness: Brightness.dark,
+//       primaryColor: Colors.deepPurple,
+//       hintColor: Colors.purpleAccent,
+//       scaffoldBackgroundColor: const Color(0xFF121212), // Fondo más elegante
+//       appBarTheme: const AppBarTheme(
+//         color: Colors.deepPurple,
+//         elevation: 2, // Sombras más ligeras
+//         iconTheme: IconThemeData(color: Color(0xFFD87FDC)),
+//         actionsIconTheme: IconThemeData(color: Color(0xFFEF5350)),
+//         toolbarTextStyle: TextStyle(color: Color(0xFF8E8EFA)),
+//         titleTextStyle: TextStyle(
+//           color: Color.fromARGB(255, 152, 238, 241),
+//           fontSize: 22,
+//           fontWeight: FontWeight.w600,
+//         ),
+//       ),
+//       textTheme: const TextTheme(
+//         displayLarge: TextStyle(
+//           color: Colors.white,
+//           fontSize: 32,
+//           fontWeight: FontWeight.bold,
+//           letterSpacing: 1.2,
+//         ),
+//         displayMedium: TextStyle(
+//           color: Colors.white70,
+//           fontSize: 24,
+//           fontWeight: FontWeight.w600,
+//         ),
+//         bodyLarge: TextStyle(color: Colors.white, fontSize: 16),
+//         bodyMedium: TextStyle(color: Colors.white60, fontSize: 14),
+//         bodySmall: TextStyle(color: Colors.white54, fontSize: 12),
+//       ),
+//       elevatedButtonTheme: ElevatedButtonThemeData(
+//         style: ElevatedButton.styleFrom(
+//           foregroundColor: Colors.white,
+//           backgroundColor: Colors.deepPurple,
+//           shape: RoundedRectangleBorder(
+//             borderRadius: BorderRadius.circular(12),
+//           ),
+//           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+//         ),
+//       ),
+//       floatingActionButtonTheme: const FloatingActionButtonThemeData(
+//         backgroundColor: Colors.deepPurpleAccent,
+//         foregroundColor: Colors.white,
+//         elevation: 2,
+//       ),
+//       inputDecorationTheme: InputDecorationTheme(
+//         filled: true,
+//         fillColor: const Color(0xFF1E1E1E),
+//         border: OutlineInputBorder(
+//           borderSide: BorderSide(color: Colors.deepPurple.shade200),
+//           borderRadius: BorderRadius.circular(12),
+//         ),
+//         focusedBorder: OutlineInputBorder(
+//           borderSide: BorderSide(color: Colors.purpleAccent),
+//           borderRadius: BorderRadius.circular(12),
+//         ),
+//         hintStyle: const TextStyle(color: Colors.white60),
+//         labelStyle: const TextStyle(color: Colors.white70),
+//       ),
+//       cardTheme: CardTheme(
+//         color: const Color(0xFF1D1D1D),
+//         elevation: 4,
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.circular(16),
+//         ),
+//         margin: const EdgeInsets.all(12),
+//       ),
+//       bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+//         backgroundColor: Color(0xFF121212),
+//         selectedItemColor: Colors.purpleAccent,
+//         unselectedItemColor: Colors.white54,
+//         elevation: 8,
+//       ),
+//     );
+//   }
+// }
+
+// class ModoLuz {
+//   static ThemeData get() {
+//     return ThemeData(
+//       brightness: Brightness.light,
+//       primaryColor: Colors.blue,
+//       hintColor: Colors.lightBlueAccent,
+//       scaffoldBackgroundColor: const Color(0xFFF5F5F5), // Fondo más moderno
+//       appBarTheme: const AppBarTheme(
+//         color: Colors.blue,
+//         elevation: 2,
+//         iconTheme: IconThemeData(color: Colors.white),
+//         actionsIconTheme: IconThemeData(color: Colors.white),
+//         titleTextStyle: TextStyle(
+//           color: Colors.white,
+//           fontSize: 22,
+//           fontWeight: FontWeight.w600,
+//         ),
+//       ),
+//       textTheme: const TextTheme(
+//         displayLarge: TextStyle(
+//           color: Colors.black,
+//           fontSize: 32,
+//           fontWeight: FontWeight.bold,
+//           letterSpacing: 1.2,
+//         ),
+//         displayMedium: TextStyle(
+//           color: Colors.black87,
+//           fontSize: 24,
+//           fontWeight: FontWeight.w600,
+//         ),
+//         bodyLarge: TextStyle(color: Colors.black, fontSize: 16),
+//         bodyMedium: TextStyle(color: Colors.black54, fontSize: 14),
+//         bodySmall: TextStyle(color: Colors.black45, fontSize: 12),
+//       ),
+//       elevatedButtonTheme: ElevatedButtonThemeData(
+//         style: ElevatedButton.styleFrom(
+//           foregroundColor: Colors.white,
+//           backgroundColor: Colors.blue,
+//           shape: RoundedRectangleBorder(
+//             borderRadius: BorderRadius.circular(12),
+//           ),
+//           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+//         ),
+//       ),
+//       floatingActionButtonTheme: const FloatingActionButtonThemeData(
+//         backgroundColor: Colors.blueAccent,
+//         foregroundColor: Colors.white,
+//         elevation: 2,
+//       ),
+//       inputDecorationTheme: InputDecorationTheme(
+//         filled: true,
+//         fillColor: Colors.white,
+//         border: OutlineInputBorder(
+//           borderSide: BorderSide(color: Colors.blue.shade200),
+//           borderRadius: BorderRadius.circular(12),
+//         ),
+//         focusedBorder: OutlineInputBorder(
+//           borderSide: BorderSide(color: Colors.lightBlueAccent),
+//           borderRadius: BorderRadius.circular(12),
+//         ),
+//         hintStyle: const TextStyle(color: Colors.black38),
+//         labelStyle: const TextStyle(color: Colors.black),
+//       ),
+//       cardTheme: CardTheme(
+//         color: Colors.white,
+//         elevation: 4,
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.circular(16),
+//         ),
+//         margin: const EdgeInsets.all(12),
+//       ),
+//       bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+//         backgroundColor: Color.fromARGB(255, 255, 0, 0),
+//         selectedItemColor: Color.fromARGB(255, 68, 230, 255),
+//         unselectedItemColor: Color.fromARGB(137, 134, 6, 146),
+//         elevation: 8,
+//       ),
+//     );
+//   }
+// }
